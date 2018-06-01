6b83864476723c3f4c35361c36d52243082e1bbd
Abhishek Sarkar <abhisheksarka@gmail.com>
2018-06-01 16:49:31 +0530

    passing id to show

--- app/graphql/link_ql/show.rb
+++ app/graphql/link_ql/show.rb
@@ -1,7 +1,8 @@
 class LinkQl::Show < GraphQL::Function
   type Types::LinkType
+  argument :id, !types.Int
 
   def call(_obj, args, _ctx)
-    ::Link.find(1)
+    ::Link.find(args['id'])
   end
 end
