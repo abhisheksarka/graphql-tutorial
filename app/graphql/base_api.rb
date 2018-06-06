ff7e9d1f2ddfb40d338ef818173d66fd7f473df5
Abhishek Sarkar <abhisheksarka@gmail.com>
2018-06-06 08:43:38 +0530

    search api

--- app/graphql/base_api.rb
+++ app/graphql/base_api.rb
@@ -2,9 +2,10 @@ class BaseApi < GraphQL::Function
   def call(obj, args, ctx)
     begin
       action(obj, args, ctx)
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     rescue StandardError => e
-      GraphQL::ExecutionError
-      .new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+      GraphQL::ExecutionError.new("Internal server error: #{e.message}")
     end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,6 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
   field :linkIndex, function: LinkApi::Index.new
+  field :linkSearch, function: LinkApi::Search.new
   field :linkShow, function: LinkApi::Show.new
 end
